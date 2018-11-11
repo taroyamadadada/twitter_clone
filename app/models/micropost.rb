fffa960c6226ccc3c36ce4a4a5f06e7868c40c0f
taro.yamada <techacademypractice@gmail.com>
2018-11-11 13:24:26 +0000

    favorites

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,5 +1,8 @@
 class Micropost < ApplicationRecord
   belongs_to :user
   
+  has_many :favarites
+  has_many :favariteds, through: :favarites, source: :user
+  
   validates :content, presence: true, length: { maximum: 255 }
 end
